e60145e6e3c238d4bda82fb97267082313f8060c
Caelin <caelinsutch@gmail.com>
2020-05-28 22:55:29 -0700

    Added a ssr to hopefully fix some css issues on reload

---
+++ gatsby-ssr.js
@@ -0,0 +1,54 @@
+import { MDXProvider } from '@mdx-js/react';
+import React from 'react';
+import Code from './src/components/code';
+import styled from '@emotion/styled';
+
+const InlineCode = styled.code`
+  background-color: rgb(1, 22, 39);
+  color: rgb(214, 222, 235);
+  padding: 0.2rem 0.3rem;
+  font-family: 'Courier New', Courier, monospace;
+  border-radius: 4px;
+  font-weight: bold;
+`;
+
+const StyledImage = styled.img`
+  max-width: 100%;
+`;
+
+const StyledText = styled.text`
+  font-size: 16px;
+`;
+
+const StyledP = styled.p`
+  font-size: 18px;
+`;
+
+const components = {
+  pre: ({ children: { props } }) => {
+    if (props.mdxType === 'code') {
+      return (
+        <Code
+          fileName={props.metastring}
+          codeString={props.children.trim()}
+          language={props.className && props.className.replace('language-', '')}
+          {...props}
+        />
+      );
+    }
+  },
+  // eslint-disable-next-line react/display-name
+  inlineCode: (a) => {
+    return <InlineCode {...a} />;
+  },
+  // eslint-disable-next-line react/display-name
+  img: (props) => <StyledImage {...props} />,
+  // eslint-disable-next-line react/display-name
+  text: (props) => <StyledText {...props} />,
+  // eslint-disable-next-line react/display-name
+  p: (props) => <StyledP {...props} />,
+};
+
+export const wrapRootElement = ({ element }) => (
+  <MDXProvider components={components}>{element}</MDXProvider>
+);
